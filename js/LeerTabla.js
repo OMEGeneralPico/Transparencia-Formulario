8fd157fd5bea3c29786104cf12a2a41311fcd7f7
EstadisticaSeb <138214680+EstadisticaSeb@users.noreply.github.com>
2024-07-29 11:50:15 -0300

    Update LeerTabla.js

--- js/LeerTabla.js
+++ js/LeerTabla.js
@@ -4,7 +4,7 @@ async function readJson() {
 
     // Abre la solicitud
     // cuando pruebo en local es /../../../json/apis.json
-    xhr.open('GET', '../json/apis.json');
+    xhr.open('GET', 'https://omegeneralpico.github.io/Transparencia-Formulario/json/apis.json', true);
 
     // Espera a que la solicitud se complete
     const response = await new Promise((resolve, reject) => {
